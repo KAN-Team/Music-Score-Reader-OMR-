42a7a05158f67d422ff96b3607fae3bd8311a927
Kareem100 <KareemSherif210@gmail.com>
2021-12-23 16:38:58 +0200

    Adding ConvertToSegment Feature

---
+++ Source/Code/ConvertToSegment.m
@@ -0,0 +1,7 @@
+function ConvertToSegment()
+%% Converting to segments...
+    image = imread('Segments/a_temp.tiff');
+    imshow(image);
+    crop = imcrop;
+    imwrite(crop, 'croped_image.tiff');
+end
