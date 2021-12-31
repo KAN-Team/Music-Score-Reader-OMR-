d9c46b525fdccad596659e3cc1e0aa1aa51e7888
nada263 <nada263>
2021-12-31 02:16:48 +0200

    Adding Gui

--- Source/Code/main.m
+++ Source/Code/main.m
@@ -2,27 +2,12 @@ function Main()
     %% Tidying up
     clc; clear; close all;
     warning off;
-    global display_figures
+    global display_figures isRotated
     display_figures = false;
-    
-    %% Reading Image
-    original_image = imread('JingleBells.bmp'); % TwinkleTwinkleLittleStar.bmp % JingleBells.bmp
-
-    % Greying Image
-    if (ndims(original_image) == 3)
-        gray_image = rgb2gray(original_image);
-    end
-
-    % Binarizing Image
-    binarized_image = ~(imbinarize(gray_image));
-    
-    %% Dividing the music score into staves and recognising them
-    recognizedScore = ProcessStaves(binarized_image);
+    isRotated = false;
     set(0, 'Children', flipud(get(0, 'Children'))) % Reverse Figure Window Order
-    
-    %% Reshaping the data and Creating the audio sample
-    GenerateAudio(recognizedScore);
-    
+    %% Run Project
+     Gui(); 
     %% Finale
     disp("====================================");
     disp("Finished !");
